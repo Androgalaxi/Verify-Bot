46dcaff2ac7811048cc2e2c31d8d5eb040c8df65
Androgalaxi <149750952+Androgalaxi@users.noreply.github.com>
2025-04-22 17:31:09 -0400

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,8 @@
+@echo off
+title Discord Bot - Always Online
+echo Starting the bot...
+:loop
+python (insert your file path to the bat file here)
+echo Bot has stopped. Restarting in 60 seconds...
+timeout /t 60 >nul
+goto loop
