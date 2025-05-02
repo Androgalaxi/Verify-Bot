5e2e5ed09bd4e9b2f4466abd637c3e53e35b4a7e
Androgalaxi <149750952+Androgalaxi@users.noreply.github.com>
2025-05-02 10:36:00 -0400

    Update start.bat

--- start.bat
+++ start.bat
@@ -2,7 +2,7 @@
 title Discord Bot - Always Online
 echo Starting the bot...
 :loop
-python (insert your file path to the bat file here)
+python (insert your file path to the bot.py file here)
 echo Bot has stopped. Restarting in 60 seconds...
 timeout /t 60 >nul
 goto loop
